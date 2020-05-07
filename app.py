e2f8c7ad487fe7d954cdecd21d4cc61d03eab559
Qipei Mei <qipei@ualberta.ca>
2020-05-07 16:43:07 -0600

    fix relatie import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from .database.models import *
+from database.models import *
 
 def create_app(test_config=None):
   # create and configure the app
